fb5bd8149758e5b1b95b2ce7e3d365530eed07d9
Max Miliano <milianordelcanto@gmail.com>
2021-12-15 16:44:12 -0300

    vercel.json was added

--- src/app/services/Axios.js
+++ src/app/services/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.API_URL ?? 'http://localhost:8080',
 });
 
 export default class Axios {
